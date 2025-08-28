e4243cbc18f330633e51629be88dd2460ce75a31
olenevoid <arnafas@gmail.com>
2025-08-28 11:30:44 +0500

    Добавил функцию на поиск по ключу в словаре

--- demo_data/utils.py
+++ demo_data/utils.py
@@ -11,3 +11,8 @@ def load_from_json(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         data = file.read()
         return json.loads(data)
+
+
+def find_value_in_dict(pk, json_filepath):
+    dictionary = load_from_json(json_filepath)
+    return dictionary.get(str(pk))
