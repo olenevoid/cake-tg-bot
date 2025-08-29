a81e3c764600e7554fd5023c7d57e7b486059d85
olenevoid <arnafas@gmail.com>
2025-08-29 10:07:28 +0500

    Перенес статичные кнопки в отдельный файл

--- tg_bot/keyboards.py
+++ tg_bot/keyboards.py
@@ -2,51 +2,26 @@ from telegram import InlineKeyboardMarkup
 from tg_bot.callbacks import Callback, CallbackButton
 from utils import split_to_sublists
 from demo_data.models import User, Cake
+import tg_bot.buttons as static_buttons
 
 
 def get_main_menu(user: User):
     buttons = [
-        CallbackButton(
-            'Заказать торт',
-            Callback.ORDER_CAKE
-        ),
-        CallbackButton(
-            'Показать цены',
-            Callback.SHOW_PRICELIST
-        ),
+        static_buttons.ORDER_CAKE,
+        static_buttons.SHOW_PRICELIST,
     ]
 
     if not user:
-        buttons.append(
-            CallbackButton(
-                'Регистрация',
-                Callback.SIGNUP
-            )
-        )
+        buttons.append(static_buttons.SIGNUP)
 
     if user and user.role.title == "Customer":
-        buttons.append(
-            CallbackButton(
-                'Мои заказы',
-                Callback.MY_ORDERS
-            )
-        )
+        buttons.append(static_buttons.MY_ORDERS)
 
     if user and user.role.title == "Admin":
-        buttons.append(
-            CallbackButton(
-                'Заказы',
-                Callback.ALL_ORDERS
-            )
-        )
+        buttons.append(static_buttons.ALL_ORDERS)
 
     if user:
-        buttons.append(
-            CallbackButton(
-                'Удалить пользователя (для тестов)',
-                Callback.DELETE_USER
-            )
-        )
+        buttons.append(static_buttons.DELETE_USER)
 
     buttons = split_to_sublists(buttons, 1)
 
@@ -55,12 +30,7 @@ def get_main_menu(user: User):
 
 def get_back_to_menu():
     buttons = [
-        [
-            CallbackButton(
-                'В меню',
-                Callback.MAIN_MENU
-            )
-         ]
+        [static_buttons.MAIN_MENU]
     ]
 
     return InlineKeyboardMarkup(buttons)
@@ -68,25 +38,14 @@ def get_back_to_menu():
 
 def get_personal_data_keyboard():
     buttons = [
-        CallbackButton(
-            'Да',
-            Callback.YES
-        ),
-        CallbackButton(
-            'Нет',
-            Callback.NO
-        )
+        static_buttons.YES,
+        static_buttons.NO
     ]
     
     buttons = split_to_sublists(buttons, 2)
     
     buttons.append(
-        [
-            CallbackButton(
-                'Скачать',
-                Callback.DOWNLOAD
-            )
-        ]
+        [static_buttons.DOWNLOAD]
     )
     
     return InlineKeyboardMarkup(buttons)
@@ -94,52 +53,26 @@ def get_personal_data_keyboard():
     
 def get_confirm_registration():
     buttons = [
-        CallbackButton(
-            'Да',
-            Callback.YES
-        ),
-        CallbackButton(
-            'Изменить',
-            Callback.REDO
-        ),
+        static_buttons.YES,
+        static_buttons.REDO,
     ]
     
     buttons = split_to_sublists(buttons, 2)
     
-    buttons.append(
-        [
-            CallbackButton(
-                'В меню',
-                Callback.MAIN_MENU
-            )
-        ]
-    )
+    buttons.append([static_buttons.MAIN_MENU])
     
     return InlineKeyboardMarkup(buttons)
 
 
 def get_order_cake():
     buttons = [
-        CallbackButton(
-            'Готовые торты',
-            Callback.SHOW_CAKES
-        ),
-        CallbackButton(
-            'Заказать новый (не работает)',
-            Callback.MAIN_MENU
-        )
+        static_buttons.SHOW_CAKES,
+        static_buttons.CUSTOM_CAKE
     ]
 
     buttons = split_to_sublists(buttons, 2)
 
-    buttons.append(
-        [
-            CallbackButton(
-                'В меню',
-                Callback.MAIN_MENU
-            )
-        ]
-    )
+    buttons.append([static_buttons.MAIN_MENU])
 
     return InlineKeyboardMarkup(buttons)
 
@@ -158,14 +91,7 @@ def get_select_cake(cakes: list[Cake], cakes_per_row: int = 2):
 
     buttons = split_to_sublists(buttons, cakes_per_row)
 
-    buttons.append(
-        [
-            CallbackButton(
-                'В меню',
-                Callback.MAIN_MENU
-            )
-        ]
-    )
+    buttons.append([static_buttons.MAIN_MENU])
 
     return InlineKeyboardMarkup(buttons)
 
@@ -186,14 +112,7 @@ def get_cake_menu(cake: Cake):
 
     buttons = split_to_sublists(buttons, 1)
 
-    buttons.append(
-        [
-            CallbackButton(
-                'В меню',
-                Callback.MAIN_MENU
-            )
-        ]
-    )
+    buttons.append([static_buttons.MAIN_MENU])
 
     return InlineKeyboardMarkup(buttons)
 
